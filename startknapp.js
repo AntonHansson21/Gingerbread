48338bae102e390ed2dc31d44a49f492485aad9f
Hilda Bergendorff <hbergendorff@gmail.com>
2018-01-19 08:36:03 +0100

    added startbuttons

--- startknapp.js
+++ startknapp.js
@@ -3,5 +3,15 @@ $(document).ready(function () {
         $('svg').css("animation-duration", "10s")
         $('svg').css("animation-play-state", "running")
         $('#startbutton').css("display", "none");
+
+    });    $("#medium").click(function() {
+        $('svg').css("animation-duration", "8s")
+        $('svg').css("animation-play-state", "running")
+        $('#medium').css("display", "none");
+
+    });    $("#hard").click(function() {
+        $('svg').css("animation-duration", "7s")
+        $('svg').css("animation-play-state", "running")
+        $('#hard').css("display", "none");
     });
 })
